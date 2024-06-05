f929ffb9349a428749d4963ca6992da0bc2e234b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-05 08:16:20 +0200

    Feat: route comments under posts + query from DB

--- blog-api-backend/controllers/postsController.js
+++ blog-api-backend/controllers/postsController.js
@@ -1,14 +1,21 @@
 const Post = require("../models/posts");
+const Comment = require("../models/comments");
 const asyncHandler = require("express-async-handler");
 
-exports.allposts_get = (req, res, next) => {
-  const post = [{ id: 1, test: "Here" }];
+exports.allposts_get = asyncHandler(async (req, res, next) => {
+  const allPosts = await Post.find({}).exec();
 
-  return res.json(post);
-};
+  return res.json(allPosts);
+});
 
 exports.post_get = asyncHandler(async (req, res, next) => {
-  const post = await Post.findById(req.params.id).exec();
+  const post = await Post.findById(req.params.postId).exec();
 
   return res.json(post);
+});
+
+exports.post_comment_get = asyncHandler(async (req, res, next) => {
+  const comments = await Comment.find({}).exec();
+
+  return res.json(comments);
 });
--- blog-api-backend/routes/posts.js
+++ blog-api-backend/routes/posts.js
@@ -7,4 +7,6 @@ router.get("/", posts_controller.allposts_get);
 
 router.get("/:postId", posts_controller.post_get);
 
+router.get("/:postId/comments", posts_controller.post_comment_get);
+
 module.exports = router;

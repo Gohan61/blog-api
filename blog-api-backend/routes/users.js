5f6ecd671d393604de7dfd81d654e81b440c5adf
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-05 16:24:22 +0200

    Fix: send post request for signup from /signup

--- blog-api-backend/routes/users.js
+++ blog-api-backend/routes/users.js
@@ -5,4 +5,6 @@ const user_controller = require("../controllers/UserController");
 
 router.get("/:authorId", user_controller.author_get);
 
+router.post("/", user_controller.user_create);
+
 module.exports = router;

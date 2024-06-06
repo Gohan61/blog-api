67d0947092885b166d7402c46e96746692ba9ca2
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-06 08:02:12 +0200

    Feat: route + control to home page

--- blog-api-backend/app.js
+++ blog-api-backend/app.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
+app.use("/", routes.home);
 app.use("/posts", routes.posts);
 app.use("/users", routes.users);
 
--- blog-api-backend/routes/index.js
+++ blog-api-backend/routes/index.js
@@ -1,4 +1,5 @@
+const home = require("./home");
 const posts = require("./posts");
 const users = require("./users");
 
-module.exports = { posts, users };
+module.exports = { posts, users, home };

9b6c593cd78c53be3a7893a71415eeb615969c4e
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-07 16:17:16 +0200

    Feat: define rout paths for react router

--- blog-user-frontend/src/main.jsx
+++ blog-user-frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignIn from "./SignIn.jsx";
+import HomePage from "./HomePage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "signin", element: <SignIn /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

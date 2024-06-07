3ca4b38aa04d048d76f1df74b7a6c76b02f6412d
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-07 07:57:32 +0200

    Feat: issue token to authors logging in + check
    * Users that try to login in the admin dashboard for creating posts now
    have a check if they are an author
    * On login they are also issued a JWT like on the normal user login page

--- blog-api-backend/controllers/AdminController.js
+++ blog-api-backend/controllers/AdminController.js
@@ -1,13 +1,41 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const User = require("../models/users");
 
 exports.admin_login = [
   body("username").trim().escape(),
   body("password").trim().escape(),
 
-  passport.authenticate("local", {
-    successRedirect: "/admin",
-    failureRedirect: "/admin",
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const userDB = await User.findOne(req.user, "author").exec();
+
+    if (!errors.isEmpty()) {
+      res.json({ message: "Validation Failed", errors: errors.array() });
+      return;
+    }
+    passport.authenticate("local", { session: false }, (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        res.json({ error: "User not found", user: user });
+      } else if (!userDB.author) {
+        res.json({ message: "You are not an author" });
+      } else {
+        req.login(user, { session: false });
+        jwt.sign(
+          { user: user },
+          "secret",
+          { expiresIn: "10d" },
+          (err, token) => {
+            if (err) console.log(err);
+            res.status(200).json({ token: token });
+          }
+        );
+      }
+    })(req, res, next);
   }),
 ];

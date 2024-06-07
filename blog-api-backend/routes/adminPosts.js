a4322034cd6cc67592d46277c5043d55cf32e378
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-07 07:47:08 +0200

    Feat: protect author post, put, delete routes
    * Only users that are logged in can access the routes for creating,
    updating or deleting posts

--- blog-api-backend/routes/adminPosts.js
+++ blog-api-backend/routes/adminPosts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const posts_controller = require("../controllers/postsController");
 
@@ -7,10 +8,22 @@ router.get("/", posts_controller.allposts_get);
 
 router.get("/:postId", posts_controller.post_get);
 
-router.post("/", posts_controller.post_create);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  posts_controller.post_create
+);
 
-router.put("/:postId", posts_controller.post_update);
+router.put(
+  "/:postId",
+  passport.authenticate("jwt", { session: false }),
+  posts_controller.post_update
+);
 
-router.delete("/:postId", posts_controller.post_delete);
+router.delete(
+  "/:postId",
+  passport.authenticate("jwt", { session: false }),
+  posts_controller.post_delete
+);
 
 module.exports = router;

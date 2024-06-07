6ed40e46847887bd1689d33881bc5e21389b5319
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-07 16:18:14 +0200

    Feat: show name of Blog in title

--- blog-api-backend/routes/home.js
+++ blog-api-backend/routes/home.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => res.json("Home"));
+router.get("/", (req, res, next) => res.json("The Blog"));
 
 module.exports = router;

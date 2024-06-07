8d54f2ec24f43ca8df819a653fc01be87cd3b214
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-07 07:23:34 +0200

    Feat: protect commment write route with JWT
    * When a user is not logged in and has no token, user can't access the
    page for writing comments

--- blog-api-backend/routes/comments.js
+++ blog-api-backend/routes/comments.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const comments_controller = require("../controllers/CommentsController");
 
-router.post("/:postId", comments_controller.new_comment);
+router.post(
+  "/:postId",
+  passport.authenticate("jwt", { session: false }),
+  comments_controller.new_comment
+);
 
 module.exports = router;

26f7c13847bfd20fb51872e974d77829cb4716ce
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-07 16:21:48 +0200

    Feat: allow cors access for frontend
    * For development cors is enabled for all routes

--- blog-api-backend/app.js
+++ blog-api-backend/app.js
@@ -6,12 +6,14 @@ const logger = require("morgan");
 const routes = require("./routes");
 require("./config/database");
 require("./config/passport");
+const cors = require("cors");
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

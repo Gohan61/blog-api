15fc386e6e3778f5609aabf288b4148f8d1f116f
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-10 14:57:09 +0200

    Refactor: change minimum comment length to 2 char

--- blog-api-backend/models/comments.js
+++ blog-api-backend/models/comments.js
@@ -5,7 +5,7 @@ const CommentsSchema = new Schema({
   userID: { type: Schema.Types.ObjectId, ref: "User", required: true },
   username: { type: Schema.Types.String, ref: "User", required: true },
   timestamp: { type: Date, required: true },
-  text: { type: String, required: true, minLength: 1, maxLength: 30 },
+  text: { type: String, required: true, minLength: 2, maxLength: 30 },
   postID: { type: Schema.Types.ObjectId, ref: "Post", required: true },
 });
 
